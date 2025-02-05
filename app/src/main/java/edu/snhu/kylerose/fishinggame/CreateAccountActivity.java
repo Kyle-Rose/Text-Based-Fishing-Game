c6bd5893ea2a0ac14134defdc786b555e418083f
Kyle-Rose <kyle.rose1@snhu.edu>
2025-02-05 15:59:38 -0500

    Baseline enhanced artifact - static application

---
+++ app/src/main/java/edu/snhu/kylerose/fishinggame/CreateAccountActivity.java
@@ -0,0 +1,16 @@
+package edu.snhu.kylerose.fishinggame;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_create_account);
+    }
+}
---
+++ app/src/main/java/edu/snhu/kylerose/fishinggame/LoginActivity.java
@@ -0,0 +1,38 @@
+package edu.snhu.kylerose.fishinggame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        Button login_button = findViewById(R.id.loginButton);
+        Button createAccount_button = findViewById(R.id.createAccountButton);
+
+        // take user to main screen
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
+        // take user to create account screen
+        createAccount_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, CreateAccountActivity.class));
+            }
+        });
+    }
+}
